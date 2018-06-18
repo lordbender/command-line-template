25b3fa0a650bb7f67f55622df4a6b89acc7d26af
Sean Willison <sean.willison@bcbsfl.com>
2018-06-18 14:57:36 -0400

    place holder

--- api/index.js
+++ api/index.js
@@ -1,6 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+require("dotenv").config();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const { HOST, PORT, BASE_ROUTE } = process.env;
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+class App {
+  constructor() {
+    this.baseRoute = BASE_ROUTE || "/utils/api/v1";
+  }
+
+  init() {
+    const app = express();
+    this.middlewareConfig(app);
+    this.routesConfig(app);
+    this.startServer(app);
+  }
+
+  middlewareConfig(app) {}
+
+  routesConfig(app) {
+    app.get("/", (req, res) => res.send("Cool"));
+  }
+
+  startServer(app) {
+    const host = HOST || "0.0.0.0";
+    const port = PORT || 8080;
+    // eslint-disable-next-line no-console
+    app.listen(port, host, () =>
+      console.log(`Example app listening on port ${host}:${port}!`)
+    );
+  }
+}
+
+// Leave this alone
+const app = new App();
+app.init();
