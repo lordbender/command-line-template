f1acf8f02aa4584f9592620782885449d9888656
Sean Willison <sean.willison@bcbsfl.com>
2018-06-19 17:51:18 -0400

    working on tests

--- test/photos.tests.js
+++ test/photos.tests.js
@@ -1,8 +1,23 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+require("babel-polyfill");
+const assert = require("assert");
+const PhotosService = require("../build/cli/services/photosService");
+
+describe("Array", function() {
+  describe("#indexOf()", function() {
+    it("should return -1 when the value is not present", function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+// TODO: Mock / Stub this axios call
+describe("PhotosService", function() {
+  describe("fetch list of photos", function() {
+    it("should return a list of photos", async () => {
+      const photosService = new PhotosService.default();
+      const photos = await photosService.list();
+
+      assert.equal(photos.length, 5000);
     });
   });
 });
